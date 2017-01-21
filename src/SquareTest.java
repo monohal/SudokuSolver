5e8a36c4ccfda8e19608cebc1a63273d50f04b36
monohal <monomonocasino@gmail.com>
2017-01-22 03:34:59 +0900

    deleteSquareNumberテスト作成

--- src/SquareTest.java
+++ src/SquareTest.java
@@ -9,7 +9,31 @@ public class SquareTest {
 
 	@Test
 	public void checkInitSquare(){
-		  Square square = new Square();
-		  assertEquals(new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9)), square.getSquare());
+		Square square = new Square();
+		assertEquals(new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9)), square.getSquare());
+	}
+
+	@Test
+	public void deleteSquareNumber(){
+		Square square = new Square();
+		square.delete(3);
+		square.delete(4);
+
+		assertEquals(new ArrayList<Integer>(Arrays.asList(1,2,5,6,7,8,9)),
+				square.getSquare());
+
+		square.delete(Arrays.asList(8,9));
+		assertEquals(new ArrayList<Integer>(Arrays.asList(1,2,5,6,7)),
+				square.getSquare());
+
+		square.delete(3);
+		assertEquals(new ArrayList<Integer>(Arrays.asList(1,2,5,6,7)),
+				square.getSquare());
+
+		square.delete(Arrays.asList(1,9));
+		assertEquals(new ArrayList<Integer>(Arrays.asList(2,5,6,7)),
+				square.getSquare());
+
+
 	}
 }
