0514223e4b79e3c330a126529a1e953cc4371714
monohal <monomonocasino@gmail.com>
2017-01-21 21:52:52 +0900

    initSquare, getSquare 実装

--- src/Square.java
+++ src/Square.java
@@ -1,4 +1,20 @@
+import java.util.ArrayList;
 
 public class Square {
+	ArrayList<Integer> square;
+	public Square(){
+		this.square = initSquare();
+	}
 
+	public ArrayList<Integer> initSquare(){
+		ArrayList<Integer> array = new ArrayList<Integer>();
+		for(int i = 1; i <= 9; i++){
+			array.add(i);
+		}
+		return array;
+	}
+
+	public ArrayList<Integer> getSquare(){
+		return this.square;
+	}
 }
