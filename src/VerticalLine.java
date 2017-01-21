afa9252c00b07e3b85b82c486609c2ceced447ed
monohal <monomonocasino@gmail.com>
2017-01-22 04:08:07 +0900

    VerticalLine getSquares実装

--- src/VerticalLine.java
+++ src/VerticalLine.java
@@ -1,4 +1,14 @@
+import java.util.ArrayList;
 
 public class VerticalLine {
+	ArrayList<Square> squares = new ArrayList<Square>();
+	public VerticalLine() {
+		for(int i = 0; i < 9; i++){
+			squares.add(new Square());
+		}
+	}
 
+	public ArrayList<Square> getSquares(){
+		return this.squares;
+	}
 }
