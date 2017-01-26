d65e4442756b67d2485c281e51f810b58af09968
monohal <monomonocasino@gmail.com>
2017-01-27 03:30:19 +0900

    output実装

--- src/SudokuSolver.java
+++ src/SudokuSolver.java
@@ -53,5 +53,57 @@ public class SudokuSolver {
 		return boxes.get(pos);
 	}
 
+	public void outputHorNumber(){
+		for(int i = 0; i < 9 ; i++){
+			HorizonLine horLine = getHorLine(i);
+
+			for(Square square : horLine.squares){
+				if(square.isConfirm()){
+					System.out.print("~" + square.number);
+				}else{
+					for(int num : square.getSquareArray()){
+						System.out.print(num);
+					}
+				}
+				System.out.print(",");
+			}
+			System.out.println();
+		}
+	}
+
+	public void outputVerNumber(){
+		for(int i = 0; i < 9 ; i++){
+			VerticalLine verLine = getVerLine(i);
+
+			for(Square square : verLine.squares){
+				if(square.isConfirm()){
+					System.out.print("~" + square.number);
+				}else{
+					for(int num : square.getSquareArray()){
+						System.out.print(num);
+					}
+				}
+				System.out.print(",");
+			}
+			System.out.println();
+		}
+	}
+
+	public void outputBoxNumber(){
+		for(int i = 0; i < 9 ; i++){
+			Box horLine = getBox(i);
+
+			for(Square square : horLine.squares){
+				if(square.isConfirm()){
+					System.out.print("~" + square.number);
+				}else{
+					for(int num : square.getSquareArray()){
+						System.out.print(num);
+					}
+				}
+				System.out.print(",");
+			}
+			System.out.println();
+		}
 	}
 }
