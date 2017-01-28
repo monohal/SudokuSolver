7dd176068be7a151b6b60bc4106d802e11eb3033
monohal <monomonocasino@gmail.com>
2017-01-29 02:11:04 +0900

    testSetCSV作成

---
+++ src/SudokuTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import org.junit.Test;
+
+public class SudokuTest {
+
+	@Test
+	public void testSetCSV(){
+		Sudoku sudoku = new Sudoku();
+		sudoku.readCSV("sudoku.csv");
+		sudoku.solver.outputVerNumber();
+
+		assertEquals(new HashSet<Integer>(Arrays.asList(9)),
+				sudoku.solver.getVerSquare(0, 0).getSquareSet());
+		assertEquals(new HashSet<Integer>(Arrays.asList(2,6,9)),
+				sudoku.solver.getVerSquare(2, 2).getSquareSet());
+
+		assertEquals(5, sudoku.solver.getVerSquare(8, 8).getNumber());
+		assertTrue(sudoku.solver.getVerSquare(8, 8).isConfirm());
+	}
+}
