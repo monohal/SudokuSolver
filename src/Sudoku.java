6caa8d94499b60c72c894cd176d400b8f42e7945
monohal <monomonocasino@gmail.com>
2017-01-29 02:23:27 +0900

    readCSV実装
    sudoku.csv 数独ファイル

---
+++ src/Sudoku.java
@@ -0,0 +1,43 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import com.orangesignal.csv.Csv;
+import com.orangesignal.csv.CsvConfig;
+import com.orangesignal.csv.handlers.StringArrayListHandler;
+
+public class Sudoku {
+	SudokuSolver solver;
+
+	public static void main(){
+	}
+
+	public Sudoku() {
+		solver = new SudokuSolver();
+	}
+
+	public void readCSV(String filename){
+		try {
+			List<String[]> csv;
+			csv = Csv.load(new File(filename),
+					new CsvConfig(),
+					new StringArrayListHandler());
+
+			for(int x = 0; x < 9; x++){
+				String[] strArray = csv.get(x);
+
+				for(int y = 0; y < 9; y++){
+					String num = strArray[y];
+
+					if(! num.equals("")){
+						this.solver.confirm(x, y, Integer.valueOf(num));
+					}
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		solver.sync();
+	}
+}
